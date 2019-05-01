d6b5c3e247ccf9ad61b992e9d94541fade6747a4
James Lee Scott <me@jamesscott.pw>
2019-05-01 10:42:08 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -20,8 +20,24 @@ AddEventHandler('playerConnecting', function(playername, setKickReason)
         DropPlayer(source, '[SERVER] You have been removed from the session due to your Steam name.')
         setKickReason('[SERVER] You have been removed from the session due to your Steam name.')
       else
-        
+        TriggerClientEvent('chatMessage', -1, ':fsn_server:', {255,0,0}, GetPlayerIdentifiers(source)[1]..' has disallowed characters in their Steam name but was not removed due to server configuration.')
       end
     end
+  end
+end)
+
+Citizen.CreateThread(function()
+  while check_during_playing do
+      Citizen.Wait(1)
+      for key, player in pairs(GetPlayers()) do
+          if checkName(GetPlayerName(player)) then
+             if kick_when_found then
+                TriggerClientEvent('chatMessage', -1, ':fsn_server:', {255,0,0}, GetPlayerIdentifiers(player)[1]..' was removed from the server due to disallowed characters in their Steam name.')
+                DropPlayer(player, '[SERVER] You have been removed from the session due to your Steam name.')
+              else
+                TriggerClientEvent('chatMessage', -1, ':fsn_server:', {255,0,0}, GetPlayerIdentifiers(player)[1]..' has disallowed characters in their Steam name but was not removed due to server configuration.')
+              end
+          end
+      end
   end
 end)
